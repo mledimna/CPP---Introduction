11b47a859197f6fe802e5bb2711ea9a5db2cf909
mledimna <mathieu-ledimna@outlook.com>
2023-02-01 05:56:25 +0100

    Vectors

---
+++ 2 - Vector Reference/main.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <iostream>
+
+void print_vector(std::vector<int> &_vector){
+    std::cout << "Vector content" << std::endl << "[";
+    for (int i=0; i<_vector.size(); i++) std::cout << std::to_string(_vector.at(i)) + ", ";
+    std::cout << "]" << std::endl;
+}
+
+// Declaration
+std::vector<int> myVector;
+
+int main(void){
+    // Append values to vector
+    myVector.push_back(5);
+    myVector.push_back(10);
+    myVector.push_back(15);
+    myVector.push_back(20);
+    myVector.push_back(25);
+
+    // Remove last element
+    myVector.pop_back();
+
+    // Access element
+    int firstElement = myVector[0];
+
+    // Using at method
+    int firstElement_by_at = myVector.at(0);
+
+    // Get number of elements
+    int numElements = myVector.size();
+
+    // Check if vector is empty
+    bool isEmpty = myVector.empty();
+
+    // Use iterators to traverse the elements of a vector.
+    std::vector<int>::iterator it;
+    for (it = myVector.begin(); it != myVector.end(); it++) {
+        std::cout << *it << " ";
+    }
+
+    print_vector(myVector);
+
+    return 0;
+}
