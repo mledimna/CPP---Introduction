76e70636e1cba09ee63061c2c308bf23a68a9fe3
mledimna <mathieu-ledimna@outlook.com>
2024-04-30 15:49:00 +0200

    Update on class_object

--- class_reference/class_object/main.cpp
+++ class_reference/class_object/main.cpp
@@ -4,36 +4,48 @@
 class MyClass{
 public:
     // Public attribute
-    std::string name;
+    std::string name = "default";
 
-    // Constructor
-    MyClass(const std::string _name){
-        std::cout << "Hi, i'm the constructor" << std::endl;
-        this->name = _name;
+    // Default constructor
+    MyClass(){
+        std::cout << "Hi, i'm the constructor of " << this << std::endl;
+    }
+
+    // Constructor taking name as an argument
+    // Using 'name(_name)' ensure a proper affectation of name attribute to _name parameter value
+    MyClass(const std::string _name): name(_name){
+        std::cout << "Hi, i'm the constructor of " << this << std::endl;
+    }
+
+    // Destructor
+    ~MyClass(){
+        std::cout << "I'm the destructor of " << this << ", bye !" << std::endl;
     }
 
     // Public method
     void tellName(void){
-        std::cout << "Hi, i'm the method" << std::endl;
-        std::cout << this->name << std::endl;
+        std::cout << "My name is " << this->name << ", my address is " << this << std::endl;
     }
 };
 
 int main(int, char**) {
-    // Class declaration
-    MyClass var("Class Name");
+    // Class declaration using default constructor
+    MyClass var1;
 
-    // Print name from class attribute
-    std::cout << var.name << std::endl;
+    // Class declaration using other constructor
+    MyClass var2("Class Name");
     
     // Print name using class method
-    var.tellName();
+    var1.tellName();
+    var2.tellName();
 }
 
 /*
 Output
-Hi, i'm the constructor
-Class Name        
-Hi, i'm the method
-Class Name
+Hi, i'm the constructor of 0x772bff7d0
+Hi, i'm the constructor of 0x772bff7b0
+My name is default, my address is 0x772bff7d0
+My name is Class Name, my address is 0x772bff7b0
+I'm the destructor of 0x772bff7b0, bye !
+I'm the destructor of 0x772bff7d0, bye !
 */
