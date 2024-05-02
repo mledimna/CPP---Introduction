918ac42c4549a8503fee8ef4f62fc40608edfabc
mledimna <mathieu-ledimna@outlook.com>
2024-05-02 09:27:20 +0200

    Added recieve method for COM_peripheral

--- class_reference/abstract/main.cpp
+++ class_reference/abstract/main.cpp
@@ -5,6 +5,7 @@ class COM_Peripheral{
 public:
     // Pure virtual method (by = 0)
     virtual void transmit(std::string _message) = 0;
+    virtual std::string recieve(void) = 0;
 
 protected:
     // Pure virtual methods (by = 0)
@@ -21,6 +22,12 @@ public:
         std::cout << "UART Transmit : " << _message << std::endl;
     }
 
+    std::string recieve(void) override {
+        std::string message = "Nouveau message !!";
+        std::cout << "UART recieve : " << message << std::endl;
+        return message;
+    }
+
 private:    
     void on(void) override { std::cout << "UART power on procedure" << std::endl; }
     void off(void) override { std::cout << "UART power off procedure" << std::endl; }
@@ -35,6 +42,14 @@ public:
         std::cout << "Select SPI device" << std::endl;
         std::cout << "SPI Transmit : " << _message << std::endl;
         std::cout << "Deselect SPI device" << std::endl;
+    }
+
+    std::string recieve(void) override {
+        std::string message = "Nouveau message !!";
+        std::cout << "Select SPI device" << std::endl;
+        std::cout << "SPI recieve : " << message << std::endl;
+        std::cout << "Deselect SPI device" << std::endl;
+        return message;
     }
 
 private:    
@@ -73,6 +88,9 @@ int main(int, char**) {
     // .at(0) renvoie l'élément 0 du vecteur
     peripherals.at(0)->transmit(message);
     peripherals.at(1)->transmit(message);
+
+    peripherals.at(0)->recieve();
+    peripherals.at(1)->recieve();
 
     peripherals.clear();
 }
